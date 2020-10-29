bc3255b3450467c4523fb3613e4c718f68a71709
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2020-10-29 20:40:23 +0100

    simple test working on local environment

---
+++ cypress/integration/main-page.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('main page', () => {
+    it('visit the main page', () => {
+        cy.visit('/');
+        cy.get("body")
+            .contains('average score');
+    });
+});
